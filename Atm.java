32b2094e34182b42d8a44f6c36fbf76534745d8d
NickPfeufer <nicholaspfeufer@msn.com>
2013-10-10 09:15:13 -0600

    greeting and login

---
+++ Atm.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class Atm {
+	
+	private Acount[] users = new Acount[3];
+	private boolean loggedIn = false;
+	private int loggedInAs;
+	private boolean running = true;
+	private Scanner reader = new Scanner(System.in);
+
+	public Atm(){
+		// initlize acounts
+		users[0] = new Acount("Bob", 1234, 100.0);
+		users[1] = new Acount("Alice", 3333, 250);
+		users[2] = new Acount("JT", 0001, 999999999);
+
+		// run interaction
+		String user;
+		int pin;
+
+		while (running) {
+			greeting();
+			if (!loggedIn) {
+				System.out.println("please enter your username");
+				user = reader.next(java.lang.String);
+				pin = reader.nextInt();
+				if (login(user, pin)) {
+					System.out.println("Welcome back " + users[loggedInAs].username());
+
+				}else {
+					System.out.println("invalid user name");
+					reader.next();
+				}
+			}else{
+				System.out.println();
+			}
+		}
+	}
+
+	public void greeting(){
+		if (loggedIn == true) {
+			System.out.println("User: " + users[loggedInAs].username());
+		}else {
+			System.out.println("Welcome, please enter your username and pin number");
+		}
+	}
+
+	public boolean login(String user, int pin){
+		for (int i = 0; i<users.length; i++) {
+			if (users[i].username() == user && users[i].pin() == pin) {
+				loggedInAs = i;
+				loggedIn = true;
+				return true;
+			}
+		}
+	}
+	public void logout(){
+		loggedIn = false;
+	}
+}
